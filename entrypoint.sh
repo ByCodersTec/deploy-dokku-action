b2b0a12a89299f3ffceb219f67a6b37b7dc930a9
Tobias Andre Eggers <tobiasandre@Tobiass-MacBook-Pro.local>
2020-10-08 18:04:02 -0300

    changing ssh call

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,6 @@ APP_NAME="${INPUT_PROJECT}-${APP_NAME}"
 
 CREATE_APP_COMMAND="sh ./scripts/deploy.sh $INPUT_BRANCH $INPUT_PROJECT"
 SET_VARIABLES_COMMAND="bash ./scripts/variables.sh $INPUT_PROJECT $INPUT_BRANCH"
-ssh root@$INPUT_HOST CREATE_APP_COMMAND | SET_VARIABLES_COMMAND
+ssh root@$INPUT_HOST $CREATE_APP_COMMAND | $SET_VARIABLES_COMMAND
 
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push -f dokku@"$INPUT_HOST":"$APP_NAME" "$INPUT_BRANCH":master

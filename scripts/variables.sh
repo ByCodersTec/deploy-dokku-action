28b7298ae6a485884572a17bba4816ee94a9baf7
Tobias Andre Eggers <tobiasae@gmail.com>
2021-06-15 14:44:44 -0300

    check if variable definition exist

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -6,11 +6,17 @@ if [ -z "$PROJECT_NAME" ]
 then
   echo "project name is empty"
 else
-  APP_NAME="${PROJECT_NAME,,}-${APP_NAME}"
+  APP_NAME="${PROJECT_NAME}-${APP_NAME}"
 fi
 echo $APP_NAME
-while IFS= read -r line; do
- ID=`expr index "$line" '='`
- STR=$line
- dokku config:set --no-restart $APP_NAME $line
-done < ./scripts/$PROJECT_NAME
+if [ -e ./scripts/$PROJECT_NAME ]
+then
+  echo "SETANDO VARIAVEIS..."
+  while IFS= read -r line; do
+    ID=`expr index "$line" '='`
+    STR=$line
+    dokku config:set --no-restart $APP_NAME $line
+  done < ./scripts/$PROJECT_NAME
+else
+  echo "NENHUMA VARIAVEL DE AMBIENTE A DEFINIR.."
+fi

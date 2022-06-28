16ca1f7e65103a73ea78de9f5c2b6293c953825b
Tobias Andre Eggers <tobiasae@gmail.com>
2022-06-28 20:07:42 -0300

    changing elsif condition

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,7 +50,7 @@ if [[ "$INPUT_PROJECT_TYPE" == "node" ]];then
   CREATE_APP_COMMAND="sh ./scripts/node_deploy.sh $APP_NAME"
 elif [[ "$INPUT_PROJECT_TYPE" == "python" ]];then
   CREATE_APP_COMMAND="sh ./scripts/python_deploy.sh $APP_NAME"
-elsif [[ "$INPUT_PROJECT_TYPE" == "java" ]];then
+elif [[ "$INPUT_PROJECT_TYPE" == "java" ]];then
   CREATE_APP_COMMAND="sh ./scripts/java_deploy.sh $APP_NAME"
 else
   CREATE_APP_COMMAND="sh ./scripts/deploy.sh $APP_NAME"

3f6cc13dd3aa03bc7e7cecbbb2a2b278423de7e1
alexandropatrik <alexandropatrik@gmail.com>
2024-02-12 16:13:59 -0300

    fix INPUT_WORKING_DIR if condition

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,7 @@ echo "adding host address to known hosts..."
 ssh-keyscan -t rsa "$INPUT_HOST" >> "$SSH_PATH/known_hosts"
 
 echo "INPUT_WORKING_DIR ... $INPUT_WORKING_DIR"
-if [["$INPUT_WORKING_DIR" != ""]];then
+if [[ -z ${INPUT_WORKING_DIR} ]];then
   cd "$INPUT_WORKING_DIR"
 else 
   echo "checkout git branch...$INPUT_BRANCH"

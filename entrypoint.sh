fdb6fbb60d46f2bdcbe13618f68a871fb7f7dbb7
alexandropatrik <alexandropatrik@gmail.com>
2024-02-12 16:17:34 -0300

    add INPUT_WORKING_DIR to safe directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,6 +32,7 @@ ssh-keyscan -t rsa "$INPUT_HOST" >> "$SSH_PATH/known_hosts"
 echo "INPUT_WORKING_DIR ... $INPUT_WORKING_DIR"
 if [[ -v INPUT_WORKING_DIR ]];then
   cd "$INPUT_WORKING_DIR"
+  git config --global --add safe.directory /github/workspace/$INPUT_WORKING_DIR
 else 
   echo "checkout git branch...$INPUT_BRANCH"
   git checkout "$INPUT_BRANCH"

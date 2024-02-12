79046b25d0efe121c47ccc80b64cd14f10fbbcaf
alexandropatrik <alexandropatrik@gmail.com>
2024-02-12 16:36:43 -0300

    setup build directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,6 @@ ssh-keyscan -t rsa "$INPUT_HOST" >> "$SSH_PATH/known_hosts"
 echo "INPUT_WORKING_DIR ... $INPUT_WORKING_DIR"
 if [[ -v INPUT_WORKING_DIR ]];then
   mv /github/workspace/$INPUT_WORKING_DIR /github/workspace.tmp
-  rm -rf /github/workspace/*
   cp /github/workspace.tmp/* /github/workspace -R
   rm -rf /github/workspace.tmp
 else 
